b02cf1017bd5e791f8ed170fa81cfeeb7026b607
Dani Sepriyanto <daniexp95@engineer.com>
2023-06-30 13:01:10 +0700

    To Do List App Vue CDN V.0.1

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgxr3PJXpaM1qtMoJgaf0NPatHCofWpT0",
+    authDomain: "vuejs-todolist-2a249.firebaseapp.com",
+    projectId: "vuejs-todolist-2a249",
+    storageBucket: "vuejs-todolist-2a249.appspot.com",
+    messagingSenderId: "981660798846",
+    appId: "1:981660798846:web:af46dbb895d87519312d2f"
+  };
